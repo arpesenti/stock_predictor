0a5f2f75021908c2a8c822d3d1bc59e7f0febf4b
Armando Pesenti Gritti <a.pesentigritti@mdx.ac.uk>
2016-07-07 20:33:24 +0100

    first working version with linear regression and knn

--- stock_predictor.py
+++ stock_predictor.py
@@ -11,22 +11,23 @@ import numpy as np
 
 class DataSource:
     quandl.ApiConfig.api_key = "nVDDmKVv_Gd5byZhaCu-"
-
-    labels_X = ['Open', 'High', 'Low', 'Close', 'Volume']
-    label_y = ['Adj. Close']
+    horizons = [1, 7, 14, 28]
+    labels = ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj. Close']
+    label_to_predict = ['Adj. Close']
 
     @classmethod
-    def get_data(cls, start_date, end_date, ticker, prediction_horizon=1):
+    def get_data(cls, start_date, end_date, tickers):
         try:
-            filename = "./cache/%s-%s-%s.pickle".format(ticker, start_date, end_date)
-            if os.path.isfile(filename):
-                data_frame = pd.read_pickle(filename)
-            else:
-                data_frame = quandl.get("WIKI/" + ticker, start_date=start_date, end_date=end_date)
-                data_frame.to_pickle(filename)
-            X = data_frame[cls.labels_X][:len(data_frame)-prediction_horizon]
-            y = data_frame[cls.label_y][prediction_horizon:]
-            return X, y
+            data_frames = {}
+            for ticker in tickers:
+                filename = "./cache/{}-{}-{}.pickle".format(ticker, start_date, end_date)
+                if os.path.isfile(filename):
+                    data_frame = pd.read_pickle(filename)
+                else:
+                    data_frame = quandl.get("WIKI/" + ticker, start_date=start_date, end_date=end_date)
+                    data_frame.to_pickle(filename)
+                data_frames[ticker] = data_frame[cls.labels]
+            return cls._get_X_y(data_frames)
         except quandl.errors.quandl_error.InvalidRequestError as err:
             print(err)
             print("Please check you have selected a valid time interval")
@@ -35,74 +36,94 @@ class DataSource:
             print("Stock not found. Please specify a valid ticker")
             return None
 
+    @classmethod
+    def _get_X_y(cls, data):
+        df = pd.concat(data.values(), axis=1, keys=list(data.keys()))
+        df = pd.concat([df.shift(-i) for i in [0] + cls.horizons], axis=1, keys=[0] + cls.horizons).dropna()
+        X = df[0]
+        y = pd.concat([df[i] for i in cls.horizons], axis=1, keys=cls.horizons)
+        return X, y
+
 
 class Learner:
     def __init__(self):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def train(self, X, y):
-        raise NotImplementedError()
+        self.model.fit(X, y)
 
-    def predict(self, X):
-        raise NotImplementedError()
+    def forecast(self, X, y, days):
+        last = X.tail(1)
+        remaining = days
+        while remaining > 0:
+            for h in DataSource.horizons[::-1]:
+                if remaining - h >= 0:
+                    prediction = pd.DataFrame(self.model.predict(X), columns=y.columns)
+                    last = prediction[h]
+                    remaining -= h
+                    break
+        return last
 
-    def rmse(self, predictions, targets):
+    def predict(self, X, y):
+        return pd.DataFrame(self.model.predict(X), columns=y.columns, index=y.index)
+
+    def _rmse(self, predictions, targets):
         return np.sqrt(((predictions - targets) ** 2).mean())
 
     def score(self, X, y):
-        return float(self.rmse(self.predict(X), y))
-
-    @classmethod
-    def cross_validation_score(cls, X, y, k=10):
-        mean = 0
-        n = int(len(y)/k)
-        for i in range(1, k):
-            X_train = X[:][:i*n]
-            y_train = y[:][:i*n]
-            X_test = X[:][i*n:(i+1)*n]
-            y_test = y[:][i*n:(i+1)*n]
-            model = cls()
-            model.train(X_train, y_train)
-            mean += model.score(X_test, y_test)
-        return mean/(k-1)
-
+        return self._rmse(self.predict(X, y), y)
 
 
 class LinearRegressionLearner(Learner):
     def __init__(self):
         self.model = Pipeline(steps=[('features_scaling', MinMaxScaler()), ('linear_regression', LinearRegression())])
 
-    def train(self, X, y):
-        self.model.fit(X, y)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
 
 class KNNLearner(Learner):
     def __init__(self, k=5):
         self.model = Pipeline(steps=[('features_scaling', MinMaxScaler()), ('knn', KNeighborsRegressor(n_neighbors=k))])
 
-    def train(self, X, y):
-        self.model.fit(X, y)
 
-    def predict(self, X):
-        return self.model.predict(X)
+class LearnerEvaluator:
+    @classmethod
+    def cross_validation_score(cls, learner_class, start_date, end_date, tickers, k=10):
+        X, y = DataSource.get_data(start_date, end_date, tickers)
+        mean_in_sample = 0
+        mean_out_sample = 0
+        n = int(len(y) / k)
+        for i in range(1, k):
+            X_train = X[:][:i * n]
+            y_train = y[:][:i * n]
+            X_test = X[:][i * n:(i + 1) * n]
+            y_test = y[:][i * n:(i + 1) * n]
+            learner = learner_class()
+            learner.train(X, y)
+            mean_in_sample += learner.score(X_train, y_train)
+            mean_out_sample += learner.score(X_test, y_test)
+            score_in_sample = mean_in_sample[:, :, DataSource.label_to_predict[0]] / (k - 1)
+            score_out_sample = mean_out_sample[:, :, DataSource.label_to_predict[0]] / (k - 1)
+        return score_in_sample, score_out_sample
 
 
 if __name__ == "__main__":
     start_date = datetime.datetime.strptime('01012015', "%d%m%Y").date()
     end_date = datetime.datetime.strptime('31122015', "%d%m%Y").date()
-    X, y = DataSource.get_data(start_date, end_date, "AAPL")
+    tickers = ["AAPL", "GOOG"]
+
+    X, y = DataSource.get_data(start_date, end_date, tickers)
 
     # linear regression
-    linear_regression_learner = LinearRegressionLearner()
-    linear_regression_learner.train(X, y)
-    print('Linear Regression in-sample error: ', linear_regression_learner.score(X, y))
-    print('Linear Regression out-sample error: ', LinearRegressionLearner.cross_validation_score(X, y))
+    lin_reg_score_in_sample, lin_reg_score_out_sample = LearnerEvaluator.cross_validation_score(LinearRegressionLearner, start_date, end_date, tickers)
+    print("--- Linear Regression ---")
+    print("RMSE in sample: ")
+    print(lin_reg_score_in_sample)
+    print("RMSE out sample: ")
+    print(lin_reg_score_out_sample)
 
-    # KNN
-    knn_learner = KNNLearner(k=5)
-    knn_learner.train(X, y)
-    print('KNN in-sample error: ', knn_learner.score(X, y))
-    print('KNN out-sample error: ', KNNLearner.cross_validation_score(X, y))
+    # knn
+    knn_score_in_sample, knn_score_out_sample = LearnerEvaluator.cross_validation_score(KNNLearner, start_date, end_date, tickers)
+    print("---  KNN ---")
+    print("RMSE in sample: ")
+    print(knn_score_in_sample)
+    print("RMSE out sample: ")
+    print(knn_score_out_sample)
